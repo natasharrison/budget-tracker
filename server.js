2fdf0d99fe749f829505f41935921bf1c3730d13
natasharrison <natasharrison@gmail.com>
2021-07-24 15:58:30 -0700

    server error

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
-  useFindAndModify: false, 
   useNewUrlParser: true,
   useFindAndModify: false
 });
